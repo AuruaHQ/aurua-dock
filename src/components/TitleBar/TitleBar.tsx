fd0c88f2ed1701ebba6f75c9a9942c5235f5669f
Alex <alexchavez@Alexs-MacBook-Pro.attlocal.net>
2026-06-10 13:29:23 -0700

    Add TrafficLights component with own folder structure, make bigger, and move logo closer

--- src/components/TitleBar/TitleBar.tsx
+++ src/components/TitleBar/TitleBar.tsx
@@ -1,9 +1,10 @@
+import { TrafficLights } from '../TrafficLights/TrafficLights';
 import './TitleBar.css';
 
 export function TitleBar() {
   return (
     <div className="titlebar">
-      {/* macOS traffic lights area */}
+      <TrafficLights />
     </div>
   );
 }
---
+++ src/components/TrafficLights/TrafficLights.tsx
@@ -0,0 +1,11 @@
+import './TrafficLights.css';
+
+export function TrafficLights() {
+  return (
+    <div className="traffic-lights">
+      <div className="traffic-light traffic-light-close"></div>
+      <div className="traffic-light traffic-light-minimize"></div>
+      <div className="traffic-light traffic-light-maximize"></div>
+    </div>
+  );
+}
